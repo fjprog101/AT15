e1b7061b151db248b9428e47bcc2d66491dc0843
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-08 21:03:57 -0400

    Added return value and suit of each card in a hand

--- src/test/java/org/fundacionjala/at15/katas/poker/PokerTest.java
+++ src/test/java/org/fundacionjala/at15/katas/poker/PokerTest.java
@@ -1,6 +1,11 @@
 package org.fundacionjala.at15.katas.poker;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.fundacionjala.at15.katas.pokerhands.diana.Hands;
 import org.junit.Test;
@@ -12,6 +17,64 @@ public class PokerTest {
         String blackHand[] = { "2H", "3D", "5S", "9C", "KD" };
         int result = hand.countCards(blackHand);
         assertEquals(5, result);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheCard() {
+        Hands hand = new Hands();
+        String blackHand[] = { "2H", "3D", "5S", "9C", "KD" };
+        List<String> blackHandValues = Arrays.asList("2", "3", "5", "9", "K");
+        ArrayList<String> result = hand.valueOfCards(blackHand);
+        assertEquals(blackHandValues, result);
+    }
+
+    @Test
+    public void itShouldReturnTheSuitOfTheCard() {
+        Hands hand = new Hands();
+        String blackHand[] = { "2H", "3D", "5S", "9C", "KD" };
+        List<String> blackHandValues = Arrays.asList("H", "D", "S", "C", "D");
+        ArrayList<String> result = hand.suitOfCards(blackHand);
+        assertEquals(blackHandValues, result);
+    }
+
+    @Test
+    public void itShouldReturnPair() {
+
+    }
+
+    @Test
+    public void itShouldReturnTwoPairs() {
+
+    }
+
+    @Test
+    public void itShouldReturnThreeOfAKind() {
+
+    }
+
+    @Test
+    public void itShouldReturnStraight() {
+
+    }
+
+    @Test
+    public void itShouldReturnFlush() {
+
+    }
+
+    @Test
+    public void itShouldReturnFullHouse() {
+
+    }
+
+    @Test
+    public void itShouldReturnFourOfAKind() {
+
+    }
+
+    @Test
+    public void itShouldReturnStraightFlush() {
+
     }
 
 }
