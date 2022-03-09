10a2be965eb2c6287f3b5621a25c3be88d653919
Adhemar Duran <adhemarduran@gmail.com>
2022-03-08 22:51:06 -0400

    Raise code coverage of Card and HandRanker classes

--- src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/CardTest.java
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/CardTest.java
@@ -17,10 +17,73 @@ public class CardTest {
 
     @Test
     public void itShouldCalculateValue() {
-        char suit = 'C';
-        char charValue = 'T';
-        int value = 10;
-        Card theCard = new Card(charValue, suit);
+        int value = 2;
+        Card theCard = new Card('2', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 3;
+        theCard = new Card('3', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 4;
+        theCard = new Card('4', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 5;
+        theCard = new Card('5', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 6;
+        theCard = new Card('6', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 7;
+        theCard = new Card('7', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 8;
+        theCard = new Card('8', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 9;
+        theCard = new Card('9', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 10;
+        theCard = new Card('T', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 11;
+        theCard = new Card('J', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 12;
+        theCard = new Card('Q', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 13;
+        theCard = new Card('K', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 14;
+        theCard = new Card('A', 'C');
+
+        assertEquals(value, theCard.calculateValue());
+
+        value = 0;
+        theCard = new Card('B', 'C');
 
         assertEquals(value, theCard.calculateValue());
     }
--- src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/HandRankerTest.java
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/HandRankerTest.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.at15.katas.pokerhands.adhemar;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,15 +8,26 @@ import org.junit.Test;
 public class HandRankerTest {
     @Test
     public void itShouldHaveAllCardsWithTheSameSuit() {
-        Card[] cards = {
+        Card[] cards1 = {
             new Card('3', 'D'),
             new Card('4', 'D'),
             new Card('5', 'D'),
             new Card('6', 'D'),
             new Card('7', 'D')
         };
-        HandRanker ranker = new HandRanker(cards);
+        HandRanker ranker = new HandRanker(cards1);
 
         assertTrue(ranker.isAllSameSuit());
+        
+        Card[] cards2 = {
+            new Card('3', 'D'),
+            new Card('4', 'D'),
+            new Card('5', 'C'),
+            new Card('6', 'D'),
+            new Card('7', 'D')
+        };
+        ranker = new HandRanker(cards2);
+
+        assertFalse(ranker.isAllSameSuit());
     }
 }
