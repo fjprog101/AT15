5dfbad4ae6f06702f4c915136e1d399bbbd639bf
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-03-09 07:54:40 -0400

    add classes by hand

---
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/Hand.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.at15.katas.pokerhands;
+
+import java.util.List;
+
+public abstract class Hand {
+    public abstract List<String> hand(String card1, String card2, String card3, String card4, String card5);
+}
