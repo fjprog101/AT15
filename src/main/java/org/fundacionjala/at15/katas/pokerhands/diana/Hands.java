c0063f51dac3ef97447bacbe6210177abbed2828
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-08 21:03:57 -0400

    Added return value and suit of each card in a hand

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Hands.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Hands.java
@@ -1,12 +1,30 @@
 package org.fundacionjala.at15.katas.pokerhands.diana;
 
+import java.util.ArrayList;
+
 public class Hands {
-    // private String blackHand[] = { "2H", "3D", "5S", "9C", "KD" };
-    // private String whiteHand[] = { "2C", "3H", "4S", "8C", "AH" };
 
     public int countCards(String[] hand) {
         int count = hand.length;
         return count;
+    }
+
+    public ArrayList<String> valueOfCards(String[] hand) {
+        ArrayList<String> valueOfFirstPosition = new ArrayList<String>();
+        for (int ind = 0; ind < hand.length; ind++) {
+            String valueFirst = hand[ind].substring(0, 1);
+            valueOfFirstPosition.add(valueFirst);
+        }
+        return valueOfFirstPosition;
+    }
+
+    public ArrayList<String> suitOfCards(String[] hand) {
+        ArrayList<String> valueOfSecondPosition = new ArrayList<String>();
+        for (int ind = 0; ind < hand.length; ind++) {
+            String valueSecond = hand[ind].substring(1, 2);
+            valueOfSecondPosition.add(valueSecond);
+        }
+        return valueOfSecondPosition;
     }
 
 }
