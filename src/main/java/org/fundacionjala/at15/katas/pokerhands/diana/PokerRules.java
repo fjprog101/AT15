d4b12e59528eba3e98ae2ff3914387efa8206fc1
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-09 08:44:33 -0400

    corrected checkstyle

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/PokerRules.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/PokerRules.java
@@ -3,18 +3,18 @@ package org.fundacionjala.at15.katas.pokerhands.diana;
 import java.util.ArrayList;
 
 public class PokerRules {
-    public ArrayList<Integer> numeros;
-    public ArrayList<String> suit;
-    public int range;
-    public int cardHigh;
-    public int secondCardHigh;
+    private ArrayList<Integer> numeros;
+    private ArrayList<String> suit;
+    // public int range;
+    // public int cardHigh;
+    // public int secondCardHigh;
 
     public PokerRules(ArrayList<Integer> numeros, ArrayList<String> suit) {
-        this.numeros = numeros;
-        this.suit = suit;
-        this.range = 0;
-        this.cardHigh = 0;
-        this.secondCardHigh = 0;
+        // this.numeros = numeros;
+        // this.suit = suit;
+        // this.range = 0;
+        // this.cardHigh = 0;
+        // this.secondCardHigh = 0;
 
     }
 
@@ -31,53 +31,42 @@ public class PokerRules {
 
     }
 
-    public int getRange() {
-        return range;
-    }
+    // public int getRange() {
+    // // return range;
+    // }
 
     public void highCard() {
-        range = 1;
-        cardHigh = 5;
     }
 
     public void pair() {
-        range = 2;
-        cardHigh = 5;
+
     }
 
     public void twoPair() {
-        range = 3;
-        cardHigh = 5;
-        secondCardHigh = 5;
 
     }
 
     public void threeOfAKind() {
-        range = 4;
-        cardHigh = 5;
+
     }
 
     public void straight() {
-        range = 5;
+
     }
 
     public void flush() {
-        range = 6;
 
     }
 
     public void fullHouse() {
-        range = 7;
 
     }
 
     public void fourOfAKind() {
-        range = 8;
 
     }
 
     public void straightFlush() {
-        range = 9;
 
     }
 }
