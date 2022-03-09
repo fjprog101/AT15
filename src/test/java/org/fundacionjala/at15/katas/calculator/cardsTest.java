4ea944b90ec3b2eedf8d7edcc0a8c1b661f63cd8
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-03-09 07:31:21 -0400

    refactor cardsTest fix gradle check

--- src/test/java/org/fundacionjala/at15/katas/calculator/cardsTest.java
+++ src/test/java/org/fundacionjala/at15/katas/calculator/cardsTest.java
@@ -5,16 +5,27 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.fundacionjala.at15.katas.pokerhands.CardSuit;
 import org.fundacionjala.at15.katas.pokerhands.DeckOfCards;
-//import org.fundacionjala.at15.katas.pokerhands.Cards;
 import org.junit.Test;
 
 public class cardsTest {
     @Test
     public void itShouldBeCardsClub() {
         DeckOfCards deck = new DeckOfCards();
-        List<String>  clubs = new ArrayList<>(List.of("2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC", "AC"));
+        List<String>  clubs = new ArrayList<>();
+        clubs.add("2C");
+        clubs.add("3C");
+        clubs.add("4C");
+        clubs.add("5C");
+        clubs.add("6C");
+        clubs.add("7C");
+        clubs.add("8C");
+        clubs.add("9C");
+        clubs.add("TC");
+        clubs.add("JC");
+        clubs.add("QC");
+        clubs.add("KC");
+        clubs.add("AC");
         List<String> compare = deck.clubsSuit();
         assertEquals(clubs, compare);
     }
@@ -22,7 +33,20 @@ public class cardsTest {
     @Test
     public void itShouldBeCardsDiamons() {
         DeckOfCards deck = new DeckOfCards();
-        List<String>  diamons = new ArrayList<>(List.of("2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD", "AD"));
+        List<String>  diamons = new ArrayList<>();
+        diamons.add("2D");
+        diamons.add("3D");
+        diamons.add("4D");
+        diamons.add("5D");
+        diamons.add("6D");
+        diamons.add("7D");
+        diamons.add("8D");
+        diamons.add("9D");
+        diamons.add("TD");
+        diamons.add("JD");
+        diamons.add("QD");
+        diamons.add("KD");
+        diamons.add("AD");
         List<String> compare = deck.diamondsSuit();
         assertEquals(diamons, compare);
     }
@@ -30,7 +54,20 @@ public class cardsTest {
     @Test
     public void itShouldBeCardsHearts() {
         DeckOfCards deck = new DeckOfCards();
-        List<String>  hearts = new ArrayList<>(List.of("2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH", "AH"));
+        List<String>  hearts = new ArrayList<>();
+        hearts.add("2H");
+        hearts.add("3H");
+        hearts.add("4H");
+        hearts.add("5H");
+        hearts.add("6H");
+        hearts.add("7H");
+        hearts.add("8H");
+        hearts.add("9H");
+        hearts.add("TH");
+        hearts.add("JH");
+        hearts.add("QH");
+        hearts.add("KH");
+        hearts.add("AH");
         List<String> compare = deck.heartsSuit();
         assertEquals(hearts, compare);
     }
@@ -38,7 +75,20 @@ public class cardsTest {
     @Test
     public void itShouldBeCardsSpades() {
         DeckOfCards deck = new DeckOfCards();
-        List<String>  spades = new ArrayList<>(List.of("2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS", "AS"));
+        List<String>  spades = new ArrayList<>();
+        spades.add("2S");
+        spades.add("3S");
+        spades.add("4S");
+        spades.add("5S");
+        spades.add("6S");
+        spades.add("7S");
+        spades.add("8S");
+        spades.add("9S");
+        spades.add("TS");
+        spades.add("JS");
+        spades.add("QS");
+        spades.add("KS");
+        spades.add("AS");
         List<String> compare = deck.spadesSuit();
         assertEquals(spades, compare);
     }
