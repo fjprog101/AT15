5ab340d47b2ba32f595b00d56d449b6c04ffa283
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-03-10 02:37:57 -0400

    Rename class Constans to Constants

---
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/saul/Constants.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.at15.katas.pokerhands.saul;
+
+public final class Constants {
+    static final int ONE = 1;
+    static final int TWO = 2;
+    static final int THREE = 3;
+    static final int FOUR = 4;
+    static final int FIVE = 5;
+    static final int SIX = 6;
+    static final int SEVEN = 7;
+    static final int EIGHT = 8;
+    static final int NINE = 9;
+}
