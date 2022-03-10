f90978fe1a96a0d10d13a1e4f8ddbc9ae722cb49
Adhemar Duran <adhemarduran@gmail.com>
2022-03-10 16:50:09 -0400

    Order patterns and fix pair and two pairs bug

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/adhemar/Pattern.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/adhemar/Pattern.java
@@ -11,6 +11,37 @@ public class Pattern {
         this.groups = new Grouper(this.cards).getGroups();
     }
 
+    public boolean isHighCard() {
+        if (this.groups.size() == Values.FIVE.getValue() && !isFlush()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isPair() {
+        for (Group group : this.groups) {
+            if (this.groups.size() == Values.FOUR.getValue()
+                && !isFlush()
+                && group.getQuantity() == Values.TWO.getValue()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isTwoPairs() {
+        int pairs = 0;
+        for (Group group : this.groups) {
+            if (group.getQuantity() == Values.TWO.getValue()) {
+                pairs++;
+            }
+        }
+        if (pairs == Values.TWO.getValue() && !isFlush()) {
+            return true;
+        }
+        return false;
+    }
+
     public boolean isFlush() {
         String prevSuit = this.cards[0].getSuit();
         for (Card card : this.cards) {
@@ -73,35 +104,6 @@ public class Pattern {
             if (group.getQuantity() == Values.THREE.getValue()) {
                 return true;
             }
-        }
-        return false;
-    }
-
-    public boolean isTwoPairs() {
-        int pairs = 0;
-        for (Group group : this.groups) {
-            if (group.getQuantity() == Values.TWO.getValue()) {
-                pairs++;
-            }
-        }
-        if (pairs == Values.TWO.getValue()) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isPair() {
-        for (Group group : this.groups) {
-            if (this.groups.size() == Values.FOUR.getValue() && group.getQuantity() == Values.TWO.getValue()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isHighCard() {
-        if (this.groups.size() == Values.FIVE.getValue() && !isFlush()) {
-            return true;
         }
         return false;
     }
--- src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/PatternTest.java
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/PatternTest.java
@@ -8,6 +8,149 @@ import org.junit.Test;
 
 public class PatternTest {
     @Test
+    public void itShouldCalculateAHighestCard() {
+        Card[] cards1 = {
+            new Card("3", "S"),
+            new Card("A", "D"),
+            new Card("5", "D"),
+            new Card("6", "H"),
+            new Card("Q", "D")
+        };
+        Pattern pattern = new Pattern(cards1);
+
+        assertEquals(14, pattern.calculateTheHighestCard());
+    }
+
+    @Test
+    public void itShouldBeHighCard() {
+        Card[] cards1 = {
+            new Card("4", "S"),
+            new Card("5", "C"),
+            new Card("3", "D"),
+            new Card("K", "H"),
+            new Card("A", "D")
+        };
+        Pattern pattern = new Pattern(cards1);
+
+        assertTrue(pattern.isHighCard());
+
+        Card[] cards2 = {
+            new Card("4", "S"),
+            new Card("4", "C"),
+            new Card("3", "D"),
+            new Card("K", "H"),
+            new Card("A", "D")
+        };
+        pattern = new Pattern(cards2);
+
+        assertFalse(pattern.isHighCard());
+
+        Card[] cards3 = {
+            new Card("4", "S"),
+            new Card("J", "S"),
+            new Card("3", "S"),
+            new Card("K", "S"),
+            new Card("A", "S")
+        };
+        pattern = new Pattern(cards3);
+
+        assertFalse(pattern.isHighCard());
+    }
+
+    @Test
+    public void itShouldBePair() {
+        Card[] cards1 = {
+            new Card("4", "S"),
+            new Card("5", "C"),
+            new Card("3", "D"),
+            new Card("4", "H"),
+            new Card("A", "D")
+        };
+        Pattern pattern = new Pattern(cards1);
+
+        assertTrue(pattern.isPair());
+
+        Card[] cards2 = {
+            new Card("3", "S"),
+            new Card("3", "C"),
+            new Card("3", "D"),
+            new Card("3", "H"),
+            new Card("T", "D")
+        };
+        pattern = new Pattern(cards2);
+
+        assertFalse(pattern.isPair());
+
+        Card[] cards3 = {
+            new Card("3", "S"),
+            new Card("3", "C"),
+            new Card("5", "D"),
+            new Card("5", "H"),
+            new Card("T", "D")
+        };
+        pattern = new Pattern(cards3);
+
+        assertFalse(pattern.isPair());
+
+        Card[] cards4 = {
+            new Card("3", "S"),
+            new Card("4", "C"),
+            new Card("5", "D"),
+            new Card("6", "H")
+        };
+        pattern = new Pattern(cards4);
+
+        assertFalse(pattern.isPair());
+
+        Card[] cards5 = {
+            new Card("3", "D"),
+            new Card("3", "D"),
+            new Card("5", "D"),
+            new Card("Q", "D"),
+            new Card("T", "D")
+        };
+        pattern = new Pattern(cards5);
+
+        assertFalse(pattern.isPair());
+    }
+
+    @Test
+    public void itShouldBeTwoPairs() {
+        Card[] cards1 = {
+            new Card("3", "S"),
+            new Card("5", "C"),
+            new Card("3", "D"),
+            new Card("A", "H"),
+            new Card("A", "D")
+        };
+        Pattern pattern = new Pattern(cards1);
+
+        assertTrue(pattern.isTwoPairs());
+
+        Card[] cards2 = {
+            new Card("3", "S"),
+            new Card("5", "C"),
+            new Card("3", "D"),
+            new Card("A", "H"),
+            new Card("T", "D")
+        };
+        pattern = new Pattern(cards2);
+
+        assertFalse(pattern.isTwoPairs());
+
+        Card[] cards = {
+            new Card("3", "S"),
+            new Card("T", "S"),
+            new Card("3", "S"),
+            new Card("A", "S"),
+            new Card("T", "S")
+        };
+        pattern = new Pattern(cards);
+
+        assertFalse(pattern.isTwoPairs());
+    }
+
+    @Test
     public void itShouldBeAllCardsWithTheSameSuit() {
         Card[] cards1 = {
             new Card("3", "D"),
@@ -94,20 +237,6 @@ public class PatternTest {
     }
 
     @Test
-    public void itShouldCalculateAHighestCard() {
-        Card[] cards1 = {
-            new Card("3", "S"),
-            new Card("A", "D"),
-            new Card("5", "D"),
-            new Card("6", "H"),
-            new Card("Q", "D")
-        };
-        Pattern pattern = new Pattern(cards1);
-
-        assertEquals(14, pattern.calculateTheHighestCard());
-    }
-
-    @Test
     public void itShoulBeFourOfAKind() {
         Card[] cards1 = {
             new Card("A", "S"),
@@ -191,112 +320,5 @@ public class PatternTest {
         pattern = new Pattern(cards2);
 
         assertFalse(pattern.isThreeOfAKind());
-    }
-
-    @Test
-    public void itShouldBeTwoPairs() {
-        Card[] cards1 = {
-            new Card("3", "S"),
-            new Card("5", "C"),
-            new Card("3", "D"),
-            new Card("A", "H"),
-            new Card("A", "D")
-        };
-        Pattern pattern = new Pattern(cards1);
-
-        assertTrue(pattern.isTwoPairs());
-
-        Card[] cards2 = {
-            new Card("3", "S"),
-            new Card("5", "C"),
-            new Card("3", "D"),
-            new Card("A", "H"),
-            new Card("T", "D")
-        };
-        pattern = new Pattern(cards2);
-
-        assertFalse(pattern.isTwoPairs());
-    }
-
-    @Test
-    public void itShouldBePair() {
-        Card[] cards1 = {
-            new Card("4", "S"),
-            new Card("5", "C"),
-            new Card("3", "D"),
-            new Card("4", "H"),
-            new Card("A", "D")
-        };
-        Pattern pattern = new Pattern(cards1);
-
-        assertTrue(pattern.isPair());
-
-        Card[] cards2 = {
-            new Card("3", "S"),
-            new Card("3", "C"),
-            new Card("3", "D"),
-            new Card("3", "H"),
-            new Card("T", "D")
-        };
-        pattern = new Pattern(cards2);
-
-        assertFalse(pattern.isPair());
-
-        Card[] cards3 = {
-            new Card("3", "S"),
-            new Card("3", "C"),
-            new Card("5", "D"),
-            new Card("5", "H"),
-            new Card("T", "D")
-        };
-        pattern = new Pattern(cards3);
-
-        assertFalse(pattern.isPair());
-
-        Card[] cards4 = {
-            new Card("3", "S"),
-            new Card("4", "C"),
-            new Card("5", "D"),
-            new Card("6", "H")
-        };
-        pattern = new Pattern(cards4);
-
-        assertFalse(pattern.isPair());
-    }
-
-    @Test
-    public void itShouldBeHighCard() {
-        Card[] cards1 = {
-            new Card("4", "S"),
-            new Card("5", "C"),
-            new Card("3", "D"),
-            new Card("K", "H"),
-            new Card("A", "D")
-        };
-        Pattern pattern = new Pattern(cards1);
-
-        assertTrue(pattern.isHighCard());
-
-        Card[] cards2 = {
-            new Card("4", "S"),
-            new Card("4", "C"),
-            new Card("3", "D"),
-            new Card("K", "H"),
-            new Card("A", "D")
-        };
-        pattern = new Pattern(cards2);
-
-        assertFalse(pattern.isHighCard());
-
-        Card[] cards3 = {
-            new Card("4", "S"),
-            new Card("J", "S"),
-            new Card("3", "S"),
-            new Card("K", "S"),
-            new Card("A", "S")
-        };
-        pattern = new Pattern(cards3);
-
-        assertFalse(pattern.isHighCard());
     }
 }
