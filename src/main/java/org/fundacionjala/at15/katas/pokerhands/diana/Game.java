31d54dd831a25dc3e94e85890b83071ddeeb8c38
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-11 08:21:02 -0400

    Corrected Game.java

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Game.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Game.java
@@ -1,22 +1,31 @@
 package org.fundacionjala.at15.katas.pokerhands.diana;
 
+import java.util.ArrayList;
+
 public class Game {
     private String[] blackHand = {"2H", "3D", "5S", "9C", "KD"};
     private String[] whiteHand = {"2H", "3D", "5S", "9C", "KD"};
+    private ArrayList<Integer> numeros;
+    private ArrayList<String> suits;
 
-    //Hands hands = new Hands();
-    // hands.countCards(blackHand);
-    // hands.countCards(whiteHand);
+    public void blackHand() {
+        Hands hands = new Hands();
+        hands.countCards(blackHand);
+        PokerRules pokerRulesBlack = new PokerRules(numeros, suits);
+        pokerRulesBlack.evaluateHand();
+        pokerRulesBlack.getRange();
+    }
 
-    // PokerRules pokerRulesWhite = new PokerRules(numeros, suits);
-    // pokerRulesWhite.evaluateHand();
-    // pokerRulesWhite.getRange();
-
-    // PokerRules pokerRulesBlack = new PokerRules(numeros, suits);
-    // pokerRulesBlack.evaluateHand();
-    // pokerRulesBlack.getRange();
+    public void whiteHand() {
+        Hands hands = new Hands();
+        hands.countCards(whiteHand);
+        PokerRules pokerRulesWhite = new PokerRules(numeros, suits);
+        pokerRulesWhite.evaluateHand();
+        pokerRulesWhite.getRange();
+    }
 
     public void compareHands() {
+        // compare range
 
     }
 }
