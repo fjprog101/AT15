700e6990089fee8ea0ff16f8d33cd2c2a30a337e
Jimy Tastaca <Jimy.Tastaca@fundacion-jala.org>
2022-03-11 10:01:45 -0400

    add new methods to PokerHand class

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/jimy/PokerHand.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/jimy/PokerHand.java
@@ -4,16 +4,49 @@ import java.util.Vector;
 
 public class PokerHand {
     private Vector<Card> card;
+    private int combination;
+    private int highCard;
     public PokerHand(String[] input) {
         card = new Vector(input.length);
         for (int index = 0; index < input.length; index++) {
             card.add(new Card(input[index]));
         }
+        order();
     }
     public int getValue(int index) {
         return card.get(index).getValue();
     }
     public char getSuit(int index) {
         return card.get(index).getSuit();
+    }
+    private void order() {
+        for (int index = 0; index < card.capacity(); index++) {
+            int indexOfMin = index;
+            int min = getValue(index);
+            for (int jIndex = index + 1; jIndex < card.capacity(); jIndex++) {
+                if (getValue(jIndex) < min) {
+                    min = getValue(jIndex);
+                    indexOfMin = jIndex;
+                }
+            }
+            Card aux = getCard(index);
+            card.set(index, getCard(indexOfMin));
+            card.set(indexOfMin, aux);
+        }
+    }
+    public int getCombination() {
+        return combination;
+    }
+    public int getHighCard() {
+        return highCard;
+    }
+    public Card getCard(int index) {
+        return card.get(index);
+    }
+    public void setCombination(int newcombination) {
+        combination = newcombination;
+    }
+    public void setHighCard(int index) {
+        this.highCard = index;
     }
 }
