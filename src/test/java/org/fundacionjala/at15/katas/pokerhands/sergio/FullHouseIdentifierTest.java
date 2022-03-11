0c138c90e8ca1f49baa068ffea0dda083a40fad9
Sergio Mendieta Valenzuela <Sergio.Mendieta@fundacion-jala.org>
2022-03-10 23:29:44 -0400

    FullHouseIdentifier.java and FullHouseIdentifierTest.java added

---
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/sergio/FullHouseIdentifierTest.java
@@ -0,0 +1,65 @@
+package org.fundacionjala.at15.katas.pokerhands.sergio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FullHouseIdentifierTest {
+    @Test
+    public void itShouldIdentifyFullHouse() {
+        Card card1 = new Card("2", "D");
+        Card card2 = new Card("2", "H");
+        Card card3 = new Card("2", "S");
+        Card card4 = new Card("6", "C");
+        Card card5 = new Card("6", "D");
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(card1);
+        cardList.add(card2);
+        cardList.add(card3);
+        cardList.add(card4);
+        cardList.add(card5);
+        FullHouseIdentifier fullHouseIdentificationTest = new FullHouseIdentifier(new FrequencyIdentifier(cardList));
+
+        assertTrue(fullHouseIdentificationTest.isFullHouse());
+    }
+
+    @Test
+    public void itShouldNotIdentifyFullHouseWithThreesome() {
+        Card card1 = new Card("2", "D");
+        Card card2 = new Card("2", "H");
+        Card card3 = new Card("2", "S");
+        Card card4 = new Card("3", "C");
+        Card card5 = new Card("6", "D");
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(card1);
+        cardList.add(card2);
+        cardList.add(card3);
+        cardList.add(card4);
+        cardList.add(card5);
+        FullHouseIdentifier fullHouseIdentificationTest = new FullHouseIdentifier(new FrequencyIdentifier(cardList));
+
+        assertFalse(fullHouseIdentificationTest.isFullHouse());
+    }
+
+    @Test
+    public void itShouldNotIdentifyFullHouseWithDuet() {
+        Card card1 = new Card("2", "D");
+        Card card2 = new Card("2", "H");
+        Card card3 = new Card("4", "S");
+        Card card4 = new Card("3", "C");
+        Card card5 = new Card("6", "D");
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(card1);
+        cardList.add(card2);
+        cardList.add(card3);
+        cardList.add(card4);
+        cardList.add(card5);
+        FullHouseIdentifier fullHouseIdentificationTest = new FullHouseIdentifier(new FrequencyIdentifier(cardList));
+
+        assertFalse(fullHouseIdentificationTest.isFullHouse());
+    }
+}
