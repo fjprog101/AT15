5a3d5c213312f99d91b53a3e1b0a00601f2926dd
vladimir <vla.evtm@gmail.com>
2022-03-11 01:07:16 -0400

    Add methods to obtain the tipe of hand and compare hands with tests

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/vladimir/HandsComparation.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/vladimir/HandsComparation.java
@@ -4,10 +4,53 @@ public class HandsComparation {
     private Hand blackHand;
     private Hand whiteHand;
 
+    public HandsComparation() {
+
+    }
+
     public HandsComparation(Hand newBlackHand, Hand newWhiteHand) {
         this.blackHand = newBlackHand;
         this.whiteHand = newWhiteHand;
     }
 
+    public String compare() {
+        if (handTipe(blackHand) > handTipe(whiteHand)) {
+            return "Black hand wins";
+        } else {
+            return "White hand wins";
+        }
+    }
 
+    public int handTipe(Hand hand) {
+        int value = 0;
+        HighCard highCard = new HighCard();
+        Pair pair = new Pair();
+        TwoPairs twoPairs = new TwoPairs();
+        ThreeOfAKind threeOfAKind = new ThreeOfAKind();
+        Straight straight = new Straight();
+        Flush flush = new Flush();
+        FullHouse fullHouse = new FullHouse();
+        Poker poker = new Poker();
+        StraightFlush straightFlush = new StraightFlush();
+        if (highCard.isHighCard(hand) != 0) {
+            return highCard.isHighCard(hand);
+        } else if (pair.isPair(hand) != 0) {
+            return pair.isPair(hand);
+        } else if (twoPairs.isTwoPair(hand) != 0) {
+            return twoPairs.isTwoPair(hand);
+        } else if (threeOfAKind.isThreeOfAKind(hand) != 0) {
+            return threeOfAKind.isThreeOfAKind(hand);
+        } else if (straight.isStraight(hand) != 0) {
+            return straight.isStraight(hand);
+        } else if (flush.isFlush(hand) != 0) {
+            return flush.isFlush(hand);
+        } else if (fullHouse.isFullHouse(hand) != 0) {
+            return fullHouse.isFullHouse(hand);
+        } else if (poker.isPoker(hand) != 0) {
+            return poker.isPoker(hand);
+        } else if (straightFlush.itStraightFlush(hand) != 0) {
+            return straightFlush.itStraightFlush(hand);
+        }
+        return value;
+    }
 }
---
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/vladimir/HandComparationTest.java
@@ -0,0 +1,164 @@
+package org.fundacionjala.at15.katas.pokerhands.vladimir;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HandComparationTest {
+    @Test
+    public void itShouldReturnTheValueOfTheHighCard() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("3", "D");
+        card[2] = new Card("4", "D");
+        card[3] = new Card("J", "H");
+        card[4] = new Card("A", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 1;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfThePair() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("3", "D");
+        card[2] = new Card("4", "D");
+        card[3] = new Card("2", "H");
+        card[4] = new Card("A", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 2;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheTwoPair() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("3", "D");
+        card[2] = new Card("A", "D");
+        card[3] = new Card("2", "H");
+        card[4] = new Card("A", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 3;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheThreeOfAKind() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("2", "D");
+        card[2] = new Card("4", "D");
+        card[3] = new Card("2", "H");
+        card[4] = new Card("A", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 4;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheStraight() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("3", "D");
+        card[2] = new Card("4", "D");
+        card[3] = new Card("5", "H");
+        card[4] = new Card("6", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 5;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheFlush() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("A", "C");
+        card[2] = new Card("9", "C");
+        card[3] = new Card("Q", "C");
+        card[4] = new Card("8", "C");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 6;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheFullHouse() {
+        Card[] card = new Card[5];
+        card[0] = new Card("2", "C");
+        card[1] = new Card("A", "D");
+        card[2] = new Card("A", "C");
+        card[3] = new Card("2", "H");
+        card[4] = new Card("2", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 7;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfThePoker() {
+        Card[] card = new Card[5];
+        card[0] = new Card("A", "C");
+        card[1] = new Card("A", "D");
+        card[2] = new Card("A", "H");
+        card[3] = new Card("2", "H");
+        card[4] = new Card("A", "S");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 8;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldReturnTheValueOfTheStraightFlush() {
+        Card[] card = new Card[5];
+        card[0] = new Card("10", "D");
+        card[1] = new Card("J", "D");
+        card[2] = new Card("Q", "D");
+        card[3] = new Card("K", "D");
+        card[4] = new Card("A", "D");
+        Hand hand = new Hand(card);
+        HandsComparation handsComparation = new HandsComparation();
+        int value = handsComparation.handTipe(hand);
+        int expected = 9;
+        assertEquals(expected, value);
+    }
+
+    @Test
+    public void itShouldCompareTheHands() {
+        Card[] card1 = new Card[5];
+        card1[0] = new Card("Q", "C");
+        card1[1] = new Card("Q", "D");
+        card1[2] = new Card("4", "D");
+        card1[3] = new Card("Q", "H");
+        card1[4] = new Card("Q", "S");
+        Hand blackHand = new Hand(card1);
+        Card[] card2 = new Card[5];
+        card2[0] = new Card("5", "C");
+        card2[1] = new Card("6", "C");
+        card2[2] = new Card("7", "C");
+        card2[3] = new Card("8", "C");
+        card2[4] = new Card("9", "C");
+        Hand whiteHand = new Hand(card2);
+        HandsComparation handsComparation = new HandsComparation(blackHand, whiteHand);
+        String result = handsComparation.compare();
+        String expected = "White hand wins";
+        assertEquals(expected, result);
+    }
+}
