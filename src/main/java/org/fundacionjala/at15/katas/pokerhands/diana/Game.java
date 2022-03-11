116273102507230bfd6b2c4460efcf8116120d4c
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-11 01:34:42 -0400

    Fixed checkstyle from Game.java

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Game.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/diana/Game.java
@@ -1,5 +1,22 @@
 package org.fundacionjala.at15.katas.pokerhands.diana;
 
 public class Game {
+    private String[] blackHand = {"2H", "3D", "5S", "9C", "KD"};
+    private String[] whiteHand = {"2H", "3D", "5S", "9C", "KD"};
 
+    //Hands hands = new Hands();
+    // hands.countCards(blackHand);
+    // hands.countCards(whiteHand);
+
+    // PokerRules pokerRulesWhite = new PokerRules(numeros, suits);
+    // pokerRulesWhite.evaluateHand();
+    // pokerRulesWhite.getRange();
+
+    // PokerRules pokerRulesBlack = new PokerRules(numeros, suits);
+    // pokerRulesBlack.evaluateHand();
+    // pokerRulesBlack.getRange();
+
+    public void compareHands() {
+
+    }
 }
