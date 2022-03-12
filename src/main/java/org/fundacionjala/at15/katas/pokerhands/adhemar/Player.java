3d3db641c2c54a6dd838bf12ecc0b4f6ee3fddb5
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-03-12 18:18:03 -0400

    Add a poker hand to player

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/adhemar/Player.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/adhemar/Player.java
@@ -2,12 +2,18 @@ package org.fundacionjala.at15.katas.pokerhands.adhemar;
 
 public class Player {
     private String name;
+    private PokerHand pokerHand;
 
-    public Player(String name) {
+    public Player(String name, String[] stringHand) {
         this.name = name;
+        this.pokerHand = new PokerHand(new CardGenerator(stringHand).getCards());
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public PokerHand getPokerHand() {
+        return this.pokerHand;
     }
 }
--- src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/PlayerTest.java
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/adhemar/PlayerTest.java
@@ -7,9 +7,19 @@ import org.junit.Test;
 public class PlayerTest {
     @Test
     public void itShouldHaveAName() {
+        String[] stringHand = {"2H", "KD", "JS", "QC", "AD"};
         String name = "Black";
-        Player thePlayer = new Player(name);
+        Player thePlayer = new Player(name, stringHand);
 
         assertEquals(name, thePlayer.getName());
+    }
+
+    @Test
+    public void itShouldHaveAHand() {
+        String[] stringHand = {"2H", "KD", "JS", "QC", "AD"};
+        String name = "Black";
+        Player thePlayer = new Player(name, stringHand);
+
+        assertEquals("high card", thePlayer.getPokerHand().getHand().getHandName());
     }
 }
