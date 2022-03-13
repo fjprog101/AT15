40bbf544af80c967f44cc76d297f90c00d6d00c4
Sergio Mendieta Valenzuela <Sergio.Mendieta@fundacion-jala.org>
2022-03-12 21:20:07 -0400

    Croupier.java and CroupierTest.java completed

--- src/main/java/org/fundacionjala/at15/katas/pokerhands/sergio/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/pokerhands/sergio/Main.java
@@ -5,33 +5,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Card card1Player1 = new Card("Q", "S");
-        Card card2Player1 = new Card("Q", "D");
-        Card card3Player1 = new Card("K", "D");
-        Card card4Player1 = new Card("7", "C");
-        Card card5Player1 = new Card("J", "H");
-        List<Card> cardListPlayer1 = new ArrayList<>();
-        cardListPlayer1.add(card1Player1);
-        cardListPlayer1.add(card2Player1);
-        cardListPlayer1.add(card3Player1);
-        cardListPlayer1.add(card4Player1);
-        cardListPlayer1.add(card5Player1);
-        Card card1Player2 = new Card("Q", "H");
-        Card card2Player2 = new Card("Q", "C");
-        Card card3Player2 = new Card("K", "H");
-        Card card4Player2 = new Card("5", "S");
-        Card card5Player2 = new Card("7", "D");
-        List<Card> cardListPlayer2 = new ArrayList<>();
-        cardListPlayer2.add(card1Player2);
-        cardListPlayer2.add(card2Player2);
-        cardListPlayer2.add(card3Player2);
-        cardListPlayer2.add(card4Player2);
-        cardListPlayer2.add(card5Player2);
-        Player player1 = new Player(cardListPlayer1, "White");
-        Player player2 = new Player(cardListPlayer2, "Black");
-        TieBreaker judge = new OnlyPairTieBreaker();
-        judge.theTieBreak(player1, player2);
-
 
     }
 }
--- src/test/java/org/fundacionjala/at15/katas/pokerhands/sergio/CroupierTest.java
+++ src/test/java/org/fundacionjala/at15/katas/pokerhands/sergio/CroupierTest.java
@@ -39,7 +39,271 @@ public class CroupierTest {
 
         Croupier judge = new Croupier(player1, player2);
 
-        assertEquals("White", judge.getFirstDecision());
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieStraightFlush() {
+        Card card1Player1 = new Card("7", "D");
+        Card card2Player1 = new Card("8", "D");
+        Card card3Player1 = new Card("9", "D");
+        Card card4Player1 = new Card("T", "D");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("2", "H");
+        Card card2Player2 = new Card("3", "H");
+        Card card3Player2 = new Card("4", "H");
+        Card card4Player2 = new Card("5", "H");
+        Card card5Player2 = new Card("6", "H");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTiePoker() {
+        Card card1Player1 = new Card("8", "S");
+        Card card2Player1 = new Card("8", "H");
+        Card card3Player1 = new Card("8", "C");
+        Card card4Player1 = new Card("8", "D");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("7", "H");
+        Card card2Player2 = new Card("7", "C");
+        Card card3Player2 = new Card("7", "D");
+        Card card4Player2 = new Card("7", "S");
+        Card card5Player2 = new Card("6", "H");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieFullHouse() {
+        Card card1Player1 = new Card("8", "S");
+        Card card2Player1 = new Card("8", "H");
+        Card card3Player1 = new Card("8", "C");
+        Card card4Player1 = new Card("J", "S");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("7", "H");
+        Card card2Player2 = new Card("7", "C");
+        Card card3Player2 = new Card("7", "D");
+        Card card4Player2 = new Card("6", "S");
+        Card card5Player2 = new Card("6", "H");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieFlush() {
+        Card card1Player1 = new Card("A", "S");
+        Card card2Player1 = new Card("8", "S");
+        Card card3Player1 = new Card("7", "S");
+        Card card4Player1 = new Card("J", "S");
+        Card card5Player1 = new Card("2", "S");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("7", "H");
+        Card card2Player2 = new Card("6", "H");
+        Card card3Player2 = new Card("2", "H");
+        Card card4Player2 = new Card("K", "H");
+        Card card5Player2 = new Card("T", "H");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieStraight() {
+        Card card1Player1 = new Card("A", "S");
+        Card card2Player1 = new Card("K", "H");
+        Card card3Player1 = new Card("T", "C");
+        Card card4Player1 = new Card("Q", "S");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("7", "H");
+        Card card2Player2 = new Card("6", "D");
+        Card card3Player2 = new Card("5", "S");
+        Card card4Player2 = new Card("9", "C");
+        Card card5Player2 = new Card("8", "H");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieThreesome() {
+        Card card1Player1 = new Card("A", "S");
+        Card card2Player1 = new Card("A", "H");
+        Card card3Player1 = new Card("A", "C");
+        Card card4Player1 = new Card("Q", "S");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("K", "H");
+        Card card2Player2 = new Card("6", "D");
+        Card card3Player2 = new Card("K", "S");
+        Card card4Player2 = new Card("9", "C");
+        Card card5Player2 = new Card("K", "D");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieDoublePair() {
+        Card card1Player1 = new Card("A", "S");
+        Card card2Player1 = new Card("A", "H");
+        Card card3Player1 = new Card("7", "C");
+        Card card4Player1 = new Card("7", "S");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("A", "H");
+        Card card2Player2 = new Card("6", "D");
+        Card card3Player2 = new Card("A", "C");
+        Card card4Player2 = new Card("6", "C");
+        Card card5Player2 = new Card("K", "D");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
+    }
+
+    @Test
+    public void itShouldDeclareWinnerWhiteTieSimpleHand() {
+        Card card1Player1 = new Card("A", "S");
+        Card card2Player1 = new Card("3", "H");
+        Card card3Player1 = new Card("7", "C");
+        Card card4Player1 = new Card("2", "S");
+        Card card5Player1 = new Card("J", "D");
+        List<Card> cardList1 = new ArrayList<>();
+        cardList1.add(card1Player1);
+        cardList1.add(card2Player1);
+        cardList1.add(card3Player1);
+        cardList1.add(card4Player1);
+        cardList1.add(card5Player1);
+        Card card1Player2 = new Card("T", "H");
+        Card card2Player2 = new Card("6", "D");
+        Card card3Player2 = new Card("9", "C");
+        Card card4Player2 = new Card("3", "S");
+        Card card5Player2 = new Card("8", "D");
+        List<Card> cardList2 = new ArrayList<>();
+        cardList2.add(card1Player2);
+        cardList2.add(card2Player2);
+        cardList2.add(card3Player2);
+        cardList2.add(card4Player2);
+        cardList2.add(card5Player2);
+
+        Player player1 = new Player(cardList1, "White");
+        Player player2 = new Player(cardList2, "Black");
+
+        Croupier judge = new Croupier(player1, player2);
+
+        assertEquals("White", judge.getFinalDecision());
     }
 
     @Test
@@ -72,15 +336,15 @@ public class CroupierTest {
 
         Croupier judge = new Croupier(player1, player2);
 
-        assertEquals("Black", judge.getFirstDecision());
+        assertEquals("Black", judge.getFinalDecision());
     }
 
     @Test
     public void itShouldDeclareTie() {
         Card card1Player1 = new Card("2", "D");
-        Card card2Player1 = new Card("2", "H");
+        Card card2Player1 = new Card("Q", "H");
         Card card3Player1 = new Card("2", "C");
-        Card card4Player1 = new Card("5", "D");
+        Card card4Player1 = new Card("J", "D");
         Card card5Player1 = new Card("5", "S");
         List<Card> cardList1 = new ArrayList<>();
         cardList1.add(card1Player1);
@@ -88,11 +352,11 @@ public class CroupierTest {
         cardList1.add(card3Player1);
         cardList1.add(card4Player1);
         cardList1.add(card5Player1);
-        Card card1Player2 = new Card("2", "D");
-        Card card2Player2 = new Card("2", "H");
-        Card card3Player2 = new Card("2", "C");
+        Card card1Player2 = new Card("2", "H");
+        Card card2Player2 = new Card("Q", "S");
+        Card card3Player2 = new Card("2", "S");
         Card card4Player2 = new Card("5", "D");
-        Card card5Player2 = new Card("5", "S");
+        Card card5Player2 = new Card("J", "S");
         List<Card> cardList2 = new ArrayList<>();
         cardList2.add(card1Player2);
         cardList2.add(card2Player2);
@@ -105,6 +369,6 @@ public class CroupierTest {
 
         Croupier judge = new Croupier(player1, player2);
 
-        assertEquals("Tie", judge.getFirstDecision());
+        assertEquals("Tie", judge.getFinalDecision());
     }
 }
