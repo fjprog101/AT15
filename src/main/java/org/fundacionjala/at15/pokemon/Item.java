bc5c4c0df897eb09634f4e00f5f21511ee6a0ec0
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-03-20 13:34:00 -0400

    Add abstract class Item

---
+++ src/main/java/org/fundacionjala/at15/pokemon/Item.java
@@ -0,0 +1,17 @@
+package org.fundacionjala.at15.pokemon;
+
+public abstract class Item {
+    private int cost;
+
+    public Item(int cost) {
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
