4f1600fb4c288d4ad7d48e9b0a0043b37140e71f
mauricio <mauridav377@gmail.com>
2022-03-20 22:41:41 -0400

    Added test for Healing class but stills depends of pokemon class

---
+++ src/test/java/org/fundacionjala/at15/pokemon/HealingPotionTest.java
@@ -0,0 +1,15 @@
+package org.fundacionjala.at15.pokemon;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HealingPotionTest {
+
+    @Test
+    public void itShouldGetTheCostOfAHealPotion() {
+        HealingPotion potion1 = new HealingPotion();
+        assertEquals(40, potion1.getCost());
+    }
+
+}
