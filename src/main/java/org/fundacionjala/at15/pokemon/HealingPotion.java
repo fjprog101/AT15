63e08678fd28f2cb77ea1085c280a18262c93b1b
mauricio <mauridav377@gmail.com>
2022-03-20 22:13:35 -0400

    Added classPotion but it depends from pokemon and item classes

---
+++ src/main/java/org/fundacionjala/at15/pokemon/HealingPotion.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.at15.pokemon;
+
+public class HealingPotion /*extends Item*/{
+    private int moneyCost;
+
+    public HealingPotion() {
+        this.moneyCost = 20;
+    }
+
+    /*public void restoreHpToMax(Pokemon p1) {
+
+        if(p1.getCurrentHealthPoints() < p1.getMaxHealthPoints()){
+            setCurrentHealthPointsWithPotion(p1.getMaxHealthPoints())
+        }
+    }
+
+    public int getCost() {
+        return moneyCost;
+    }*/
+}
