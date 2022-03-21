d65191afe6bbee847184b5f84559ac928ff19059
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-03-21 19:22:30 -0400

    add test hp 0

--- src/test/java/org/fundacionjala/at15/pokemon/DamageReceivedTest.java
+++ src/test/java/org/fundacionjala/at15/pokemon/DamageReceivedTest.java
@@ -13,4 +13,13 @@ public class DamageReceivedTest {
         DamageReceived damage = new DamageReceived(maxHitPoints, attackDamage);
         assertEquals(afterHp, damage.getHp());
     }
+
+    @Test
+    public void itShouldBeHpZero() {
+        int currentHitPoints = 80;
+        int attackDamage = 100;
+        int afterHp = 0;
+        DamageReceived damage = new DamageReceived(currentHitPoints, attackDamage);
+        assertEquals(afterHp, damage.getHp());
+    }
 }
