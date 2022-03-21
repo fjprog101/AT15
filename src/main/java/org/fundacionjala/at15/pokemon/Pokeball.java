6a5750af92eabd2900d9792d7b00b9da0dd957f9
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-03-21 20:30:15 -0300

    var COST is static final. Added .idea to gitignore

--- src/main/java/org/fundacionjala/at15/pokemon/Pokeball.java
+++ src/main/java/org/fundacionjala/at15/pokemon/Pokeball.java
@@ -1,9 +1,10 @@
 package org.fundacionjala.at15.pokemon;
 
 public class Pokeball extends Item {
+    static final int COST = 20;
 
     public Pokeball() {
-        this.cost = 5;
+        this.cost = COST;
     }
 
 
