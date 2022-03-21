56d78c2cc3a021750473c4aa8af7ceb2840c97d9
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-03-21 19:15:33 -0400

    refactor DamageReceived class

--- src/main/java/org/fundacionjala/at15/pokemon/DamageReceived.java
+++ src/main/java/org/fundacionjala/at15/pokemon/DamageReceived.java
@@ -4,6 +4,9 @@ public class DamageReceived {
     private int currentHitPoints;
     private int potency;
     private final int faint = 0;
+    private static final double TWENTY_PERCENT = 0.20;
+    private double twentyPercentMaxHitPoints;
+    private boolean is20Percent = false;
 
     public DamageReceived(int maxHitPoints, int potency) {
         this.currentHitPoints = maxHitPoints;
@@ -17,5 +20,16 @@ public class DamageReceived {
             currentHitPoints = faint;
         }
         return currentHitPoints;
+    }
+
+    public void twentyPercentMaxHitPoints(int maxHitPoints) {
+        twentyPercentMaxHitPoints = maxHitPoints * TWENTY_PERCENT;
+    }
+
+    public boolean twentyPercentCheck() {
+        if (currentHitPoints <= twentyPercentMaxHitPoints) {
+            is20Percent = true;
+        }
+        return is20Percent;
     }
 }
