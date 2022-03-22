a7260b8615d84f89c25e9398e50aeae1fae1ff3a
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-03-21 20:39:37 -0400

    Store class can sell pokeballs

--- src/main/java/org/fundacionjala/at15/pokemon/Store.java
+++ src/main/java/org/fundacionjala/at15/pokemon/Store.java
@@ -3,12 +3,11 @@ package org.fundacionjala.at15.pokemon;
 import java.util.ArrayList;
 
 public class Store {
-    public static final int POKEBALL_COST = 20;
 
-    // public static void buyPokeball(ArrayList<Pokeball> pokeballs, Wallet wallet) {
-    //     pokeballs.add(new Pokeball());
-    //     wallet.spendMoney(POKEBALL_COST);
-    // }
+    public static void buyPokeball(ArrayList<Pokeball> pokeballs, Wallet wallet) {
+        pokeballs.add(new Pokeball());
+        wallet.spendMoney(Pokeball.POKEBALL_COST);
+    }
 
     public static void buyHealingPotion(ArrayList<HealingPotion> potions, Wallet wallet) {
         potions.add(new HealingPotion());
