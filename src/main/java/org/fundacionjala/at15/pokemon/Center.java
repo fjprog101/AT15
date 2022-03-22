fe9e097e2256c196b130c068defdb6957c25b410
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-03-22 19:59:14 -0400

    fixing commented problems

--- src/main/java/org/fundacionjala/at15/pokemon/Center.java
+++ src/main/java/org/fundacionjala/at15/pokemon/Center.java
@@ -1,6 +1,16 @@
 package org.fundacionjala.at15.pokemon;
 
+import java.util.ArrayList;
+
 public class Center implements HPAffecter {
+    private ArrayList<Pokemon> pokemonTeam = new ArrayList<Pokemon>();
+
+    public void healTeam(PokemonTeam team) {
+        this.pokemonTeam = team.getPokemonTeam();
+        for (int ind = 0; ind < pokemonTeam.size(); ind++) {
+            affectHP(pokemonTeam.get(ind));
+        }
+    }
 
     public void affectHP(Pokemon pokemon) {
         pokemon.getHitPoints().fullRestore();
--- src/test/java/org/fundacionjala/at15/pokemon/CenterTest.java
+++ src/test/java/org/fundacionjala/at15/pokemon/CenterTest.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 public class CenterTest {
     @Test
     public void itShouldFullRestoreHP() {
-        Pokemon pokemon = new Pokemon(1000, "pokemon");
+        Pokemon pokemon = new Pokemon(1000, "name");
+        PokemonTeam team = new PokemonTeam(pokemon);
         pokemon.getHitPoints().setCurrentHitPoints(0);
-        pokemon.getHitPoints().fullRestore();
+        Center center = new Center();
+        center.healTeam(team);
         assertEquals(1000, pokemon.getHitPoints().getCurrentHitPoints());
     }
 }
