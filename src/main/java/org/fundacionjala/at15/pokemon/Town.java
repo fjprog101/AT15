bb570692c7c0b499b6a20ce5c78f16f2d8b0f3ce
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-03-22 20:12:49 -0400

    added wild pokemons, trainers and store into town class

--- src/main/java/org/fundacionjala/at15/pokemon/Town.java
+++ src/main/java/org/fundacionjala/at15/pokemon/Town.java
@@ -1,7 +1,38 @@
 package org.fundacionjala.at15.pokemon;
 
+import java.util.ArrayList;
+
 public class Town {
-    Store strore = new Store();
-    // Gym gym = new Gym();
-    // Center center = new Center();
+    private ArrayList<Pokemon> wildPokemons;
+    private ArrayList<Trainer> trainers;
+    private Store store;
+    private static final int POKEMON_HP = 100;
+    // Center center;
+    // Gym gym;
+
+    public Town() {
+        wildPokemons = new ArrayList<Pokemon>();
+        trainers = new ArrayList<Trainer>();
+        store = new Store();
+        // center = new Center();
+        // gym = new Gym();
+    }
+
+    public void addWildPokemonsToTown(int wildPokemonsTotal) {
+        for (int ind = 0; ind < wildPokemonsTotal; ind++) {
+            wildPokemons.add(new Pokemon(POKEMON_HP, "Pokemon" + ind));
+        }
+    }
+
+    public void addTrainersToTown(int trainersTotal) {
+        for (int ind = 0; ind < trainersTotal; ind++) {
+            Pokemon pokemon = new Pokemon(POKEMON_HP, "Pikachu");
+            trainers.add(new Trainer(pokemon, "Trainer" + ind));
+        }
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
 }
