bd3b3301906b9ca25ad00c862b26e75e9c5b2e62
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-03-22 21:58:38 -0400

    Add trainers into the store

--- src/main/java/org/fundacionjala/at15/pokemon/Store.java
+++ src/main/java/org/fundacionjala/at15/pokemon/Store.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 
 public class Store {
 
-    public static void buyPokeball(ArrayList<Pokeball> pokeballs, Wallet wallet) {
+    private ArrayList<Trainer> trainers;
+
+    public Store() {
+        this.trainers = new ArrayList<Trainer>();
+    }
+
+    public void buyPokeball(ArrayList<Pokeball> pokeballs, Wallet wallet) {
         pokeballs.add(new Pokeball());
         wallet.spendMoney(Pokeball.POKEBALL_COST);
     }
 
-    public static void buyHealingPotion(ArrayList<HealingPotion> potions, Wallet wallet) {
+    public void buyHealingPotion(ArrayList<HealingPotion> potions, Wallet wallet) {
         potions.add(new HealingPotion());
         wallet.spendMoney(HealingPotion.POTION_COST);
+    }
+
+    public ArrayList<Trainer> getTrainers() {
+        return this.trainers;
+    }
+
+    public void addTrainer(Trainer trainer) {
+        this.trainers.add(trainer);
     }
 }
--- src/test/java/org/fundacionjala/at15/pokemon/StoreTest.java
+++ src/test/java/org/fundacionjala/at15/pokemon/StoreTest.java
@@ -8,11 +8,22 @@ import org.junit.Test;
 
 public class StoreTest {
     @Test
+    public void itShouldHaveTrainers() {
+        Pokemon pokemon = new Pokemon(100, "pikachu");
+        Trainer trainer = new Trainer(pokemon, "Ash");
+        Store store = new Store();
+        store.addTrainer(trainer);
+
+        assertEquals("Ash", store.getTrainers().get(0).getName());
+    }
+
+    @Test
     public void itShouldGiveAPokeballAndChargeCost() {
         ArrayList<Pokeball> pokeballs = new ArrayList<Pokeball>();
         int initialSize = pokeballs.size();
         Wallet wallet = new Wallet();
-        Store.buyPokeball(pokeballs, wallet);
+        Store store = new Store();
+        store.buyPokeball(pokeballs, wallet);
 
         assertEquals(initialSize + 1, pokeballs.size());
         assertEquals(80, wallet.getMoneyInWallet());
@@ -23,7 +34,8 @@ public class StoreTest {
         ArrayList<HealingPotion> potions = new ArrayList<HealingPotion>();
         int initialSize = potions.size();
         Wallet wallet = new Wallet();
-        Store.buyHealingPotion(potions, wallet);
+        Store store = new Store();
+        store.buyHealingPotion(potions, wallet);
 
         assertEquals(initialSize + 1, potions.size());
         assertEquals(60, wallet.getMoneyInWallet());
