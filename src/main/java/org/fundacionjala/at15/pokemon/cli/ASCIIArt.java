727cc09e7531be1a03a485986274601073b245d6
Jose Guevara <jguevara@pros.com>
2022-03-25 11:46:46 -0400

    Add constants for magic numbers and fix braces not set (if/for)

--- src/main/java/org/fundacionjala/at15/pokemon/cli/ASCIIArt.java
+++ src/main/java/org/fundacionjala/at15/pokemon/cli/ASCIIArt.java
@@ -16,6 +16,15 @@ import java.awt.image.BufferedImage;
 public class ASCIIArt implements Runnable {
 
     private static final int DEFAULT_SIZE = 19;
+    private static final int DEFAULT_WIDTH = 144;
+    private static final int DEFAULT_HEIGHT = 32;
+
+    private static final int X_COORD_TO_DRAW = 6;
+    private static final int Y_COORD_TO_DRAW = 24;
+    private static final int X_INIT = 0;
+    private static final int Y_INIT = 0;
+
+    private static final int RGB_LIMIT = -16777216;
 
     @Option(names = { "-s", "--font-size" }, description = "Font size")
     private int fontSize = DEFAULT_SIZE;
@@ -26,19 +35,22 @@ public class ASCIIArt implements Runnable {
 
     @Override
     public void run() {
-        BufferedImage image = new BufferedImage(144, 32, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(DEFAULT_WIDTH, DEFAULT_HEIGHT, BufferedImage.TYPE_INT_RGB);
         Graphics graphics = image.getGraphics();
         graphics.setFont(new Font("Dialog", Font.PLAIN, fontSize));
         Graphics2D graphics2D = (Graphics2D) graphics;
         graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics2D.drawString(String.join(" ", words), 6, 24);
+        graphics2D.drawString(String.join(" ", words), X_COORD_TO_DRAW, Y_COORD_TO_DRAW);
 
-        for (int y = 0; y < 32; y++) {
+        for (int yIndex = Y_INIT; yIndex < DEFAULT_HEIGHT; yIndex++) {
             StringBuilder builder = new StringBuilder();
-            for (int x = 0; x < 144; x++)
-                builder.append(image.getRGB(x, y) == -16777216 ? " " : image.getRGB(x, y) == -1 ? "#" : "*");
-            if (builder.toString().trim().isEmpty()) continue;
+            for (int xIndex = X_INIT; xIndex < DEFAULT_WIDTH; xIndex++) {
+                builder.append(image.getRGB(xIndex, yIndex) == RGB_LIMIT ? " " : image.getRGB(xIndex, yIndex) == -1 ? "#" : "*");
+            }
+            if (builder.toString().trim().isEmpty()) {
+                continue;
+            }
             System.out.println(builder);
         }
     }
