526825292cb611589975743735d5aa6f9201eaab
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-03-27 17:54:08 -0300

    added :create command, added :pokemon & :trainer subcommands

--- src/main/java/org/fundacionjala/at15/pokemon/commands/PokemonCli.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/PokemonCli.java
@@ -2,11 +2,14 @@ package org.fundacionjala.at15.pokemon.commands;
 
 import org.fundacionjala.at15.pokemon.Pokemon;
 import org.fundacionjala.at15.pokemon.ID.Identifier;
+import org.fundacionjala.at15.pokemon.Trainer;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "pkm admin", subcommands = { PokemonCommand.class,
+import java.util.concurrent.Callable;
+
+@Command(name = "pkm admin", subcommands = { SubCommandsMethods.class,
     CommandLine.HelpCommand.class }, description = "Create data for the Pokemon World")
 public class PokemonCli {
     public static void main(String[] args) {
@@ -15,23 +18,47 @@ public class PokemonCli {
     }
 }
 
-@Command(name = "pokemon", description = "Create Pokemons data")
-class PokemonCommand implements Runnable {
+@Command(name = ":create", description = "Entry point to create objects in Pokemon world")
+class SubCommandsMethods implements Callable<Integer> {
 
-    @Option(names = { "-name" }, required = true, description = "Pokemon name")
-    private String name;
+    @Command(name = ":pokemon", description = "create a pokemon")
 
-    @Option(names = { "-hitpoints" }, required = true, description = "Pokemon HitPoints")
+    @Parameters(arity = "2")
+    private String pokemonName;
     private int hitpoints;
 
-    @Override
-    public void run() {
-        Pokemon newPokemon = new Pokemon(hitpoints, name);
+    public Integer pokemon() {
+
+        Pokemon newPokemon = new Pokemon(hitpoints, pokemonName);
         String hCode = Identifier.generateIdPokemon(newPokemon);
         System.out.println(
-            "Pokemon created: \n"
-            + "Name: " + newPokemon.getPokemonName() + "\n"
-            + "HitPoints: " + newPokemon.getHitPoints().getCurrentHitPoints() + "\n"
-            + "ID: " + hCode);
+                "Pokemon created: \n"
+                        + "Name: " + newPokemon.getPokemonName() + "\n"
+                        + "HitPoints: " + newPokemon.getHitPoints().getCurrentHitPoints() + "\n"
+                        + "ID: " + hCode);
+        return 0;
+    }
+
+    @Command(name = ":trainer", description = "create a trainer")
+
+    @Parameters(arity = "2")
+    private Pokemon pokemon;
+    private String trainerName;
+
+    public Integer trainer() {
+        Trainer trainer = new Trainer(pokemon, trainerName);
+        String hCode = Identifier.generateIdTrainer(trainer);
+        System.out.println(
+                "Trainer " + trainer.getName() + " created" + "\n"
+                + pokemon.hashCode() + " added to pokemon team" + "\n"
+                + "ID: " + hCode
+        );
+        return 0;
+    }
+
+    @Override
+    public Integer call() {
+        System.out.println("Subcommand :pokemon or :trainer needed");
+        return 0;
     }
 }
