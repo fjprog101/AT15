8b6140eabbdd61a278e120aca8bb9e5b06938b2a
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-03-28 12:35:20 -0300

    fixed PokemonCommands & TrainerCommands problems

--- src/main/java/org/fundacionjala/at15/pokemon/commands/PokemonCommands.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/PokemonCommands.java
@@ -3,19 +3,16 @@ package org.fundacionjala.at15.pokemon.commands;
 import org.fundacionjala.at15.pokemon.ID.Identifier;
 import org.fundacionjala.at15.pokemon.Pokemon;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
 
-@Command(name = ":create", description = "Entry point to create objects in Pokemon world")
+@Command(name = ":pokemon", description = "Entry point to create objects in Pokemon world")
 public class PokemonCommands implements Callable<Integer> {
 
     @Option(names = {"-name", "-n"}, description = "pokemon name", defaultValue = "Pikachu")
-    @Parameters(paramLabel = "NAME")
     private String pokemonName;
 
     @Option(names = {"-hitPoints", "-hp"}, description = "pokemon Hit-Points", defaultValue = "100")
-    @Parameters(paramLabel = "HIT-POINTS")
     private int hitPoints;
 
     @Override
