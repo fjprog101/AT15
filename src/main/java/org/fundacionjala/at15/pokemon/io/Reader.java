493cbccb77f213fac4f9ea091849f60cd27ea467
Adrian Oviedo <adryoviedo@gmail.com>
2022-03-28 12:26:37 -0400

    implemented Reader's logic with Adhemar's Writer logic

---
+++ src/main/java/org/fundacionjala/at15/pokemon/io/Reader.java
@@ -0,0 +1,53 @@
+package org.fundacionjala.at15.pokemon.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.fundacionjala.at15.pokemon.Entity;
+import org.fundacionjala.at15.pokemon.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public final class Reader {
+    public static void readJson(Entity entity) {
+        JSONParser jsonParser = new JSONParser();
+        File inputFile = new File(System.getProperty("user.home") + "/" + entity.getPath().getPathString() + "/" + entity.getIdentifier() + ".json");
+
+        try (FileReader reader = new FileReader(inputFile)) {
+            Object obj = jsonParser.parse(reader);
+            parseEmployeeObject((JSONObject) obj);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        final int hitPoints = 100;
+        Pokemon pokemon = new Pokemon(hitPoints, "Pikachu");
+        Writer.writeToJson(pokemon);
+        readJson(pokemon);
+    }
+
+    private static void parseEmployeeObject(JSONObject entity) {
+        System.out.println();
+
+        String identifier = (String) entity.get("identifier");
+        System.out.println("id: " + identifier);
+
+        String name = (String) entity.get("pokemonName");
+        System.out.println("name: " + name);
+
+        JSONObject pokemonObject = (JSONObject) entity.get("hitPoints");
+        Long currentHitPoints = (Long) pokemonObject.get("currentHitPoints");
+        Long maxHitPoints = (Long) pokemonObject.get("maxHitPoints");
+        System.out.println("hitpoints: " + currentHitPoints + "/" + maxHitPoints);
+    }
+}
