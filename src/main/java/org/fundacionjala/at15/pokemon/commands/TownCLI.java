725bd5b49e87f61bed7a2d30aa73356ae02946e6
vladimir <vla.evtm@gmail.com>
2022-03-29 22:44:35 -0400

    refactor town commands

--- src/main/java/org/fundacionjala/at15/pokemon/commands/TownCLI.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/TownCLI.java
@@ -1,7 +1,7 @@
 package org.fundacionjala.at15.pokemon.commands;
 
-import org.fundacionjala.at15.pokemon.ID.Identifier;
 import org.fundacionjala.at15.pokemon.Pokemon;
+import org.fundacionjala.at15.pokemon.Town;
 import org.fundacionjala.at15.pokemon.Trainer;
 import org.fundacionjala.at15.pokemon.io.Reader;
 import org.fundacionjala.at15.pokemon.io.Writer;
@@ -13,61 +13,21 @@ import java.util.concurrent.Callable;
 public class TownCLI implements Callable<Integer> {
 
 
-    @Option(names = {"-addwpok1", "-addwpkm1"}, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon1;
+    @Option(names = {"-addwpok", "-tp"}, description = "Add a Wild Pokemon in town")
+    private String wildPokemon;
 
-    @Option(names = {"-addwpok2", "-addwpkm2"}, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon2;
-
-    @Option(names = {"-addwpok3", "-addwpkm3"}, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon3;
-
-    @Option(names = {"-addwpok4", "-addwpkm4"}, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon4;
-
-    @Option(names = {"-addtrainer1#"}, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID1;
-
-    @Option(names = {"-addtrainer2#"}, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID2;
-
-    @Option(names = {"-addtrainer3#"}, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID3;
-
-    @Option(names = {"-addtrainer4#"}, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID4;
-
-
+    @Option(names = {"-addtrainer", "-tt"}, description = "Add a trainer in town")
+    private String trainerID;
 
     @Override
     public Integer call() {
-        org.fundacionjala.at15.pokemon.Town newTown = new org.fundacionjala.at15.pokemon.Town();
-        Identifier identifier = new Identifier();
-        String hCode = identifier.generateId(newTown);
-        Pokemon poke1 = (Pokemon) Reader.readJson(wildPokemon1);
-        Pokemon poke2 = (Pokemon) Reader.readJson(wildPokemon2);
-        Pokemon poke3 = (Pokemon) Reader.readJson(wildPokemon3);
-        Pokemon poke4 = (Pokemon) Reader.readJson(wildPokemon4);
-        newTown.addWildPokemonsToTown(poke1, poke2, poke3, poke4);
-        Trainer trainer1 = (Trainer) Reader.readJson(trainerID1);
-        Trainer trainer2 = (Trainer) Reader.readJson(trainerID2);
-        Trainer trainer3 = (Trainer) Reader.readJson(trainerID3);
-        Trainer trainer4 = (Trainer) Reader.readJson(trainerID4);
-        newTown.addTrainersToTown(trainer1, trainer2, trainer3, trainer4);
+        Town newTown = new Town();
+
+        Pokemon poke = (Pokemon) Reader.readJson(wildPokemon);
+        newTown.addWildPokemonsToTown(poke);
+        Trainer trainer = (Trainer) Reader.readJson(trainerID);
+        newTown.addTrainersToTown(trainer);
         Writer.writeToJson(newTown);
-//        System.out.println("Town created: \n"
-//                + "WildPokemonsList: ");
-//        for (int ind = 0; ind < newTown.getWildPokemons().size(); ind++) {
-//            System.out.println(newTown.getWildPokemons().get(ind).getPokemonName() + " "
-//                    + newTown.getWildPokemons().get(ind).getIdentifier());
-//        }
-//        System.out.println("Trainers: ");
-//        for (int ind2 = 0; ind2 < newTown.getTrainers().size(); ind2++) {
-//            System.out.println(newTown.getTrainers().get(ind2).getName() + " "
-//                    + newTown.getTrainers().get(ind2).getIdentifier());
-//        }
-//        System.out.println("ID" + hCode + "\n"
-//                + "Store\nPokemon Center\nPokemon Gym");
 
         return 0;
     }
