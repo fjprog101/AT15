5930f5306c88a192e8edabe034d361c8e0c16f10
vladimir <vla.evtm@gmail.com>
2022-03-29 22:52:28 -0400

    resolve conflicts

--- src/main/java/org/fundacionjala/at15/pokemon/commands/TownCLI.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/TownCLI.java
@@ -12,42 +12,12 @@ import java.util.concurrent.Callable;
 @Command(name = "town", description = "Create a Pokemon Town")
 public class TownCLI implements Callable<Integer> {
 
-<<<<<<< HEAD
 
     @Option(names = {"-addwpok", "-tp"}, description = "Add a Wild Pokemon in town")
     private String wildPokemon;
 
     @Option(names = {"-addtrainer", "-tt"}, description = "Add a trainer in town")
     private String trainerID;
-=======
-    @Option(names = { "-addwpok1",
-            "-addwpkm1" }, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon1;
-
-    @Option(names = { "-addwpok2",
-            "-addwpkm2" }, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon2;
-
-    @Option(names = { "-addwpok3",
-            "-addwpkm3" }, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon3;
-
-    @Option(names = { "-addwpok4",
-            "-addwpkm4" }, description = "Add a Wild Pokemon in town", defaultValue = "pkm1334675172")
-    private String wildPokemon4;
-
-    @Option(names = { "-addtrainer1#" }, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID1;
-
-    @Option(names = { "-addtrainer2#" }, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID2;
-
-    @Option(names = { "-addtrainer3#" }, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID3;
-
-    @Option(names = { "-addtrainer4#" }, description = "Add a trainer in town", defaultValue = "trn1787913218")
-    private String trainerID4;
->>>>>>> dev/pokemon
 
     @Override
     public Integer call() {
@@ -58,22 +28,6 @@ public class TownCLI implements Callable<Integer> {
         Trainer trainer = (Trainer) Reader.readJson(trainerID);
         newTown.addTrainersToTown(trainer);
         Writer.writeToJson(newTown);
-<<<<<<< HEAD
-=======
-        // System.out.println("Town created: \n"
-        // + "WildPokemonsList: ");
-        // for (int ind = 0; ind < newTown.getWildPokemons().size(); ind++) {
-        // System.out.println(newTown.getWildPokemons().get(ind).getPokemonName() + " "
-        // + newTown.getWildPokemons().get(ind).getIdentifier());
-        // }
-        // System.out.println("Trainers: ");
-        // for (int ind2 = 0; ind2 < newTown.getTrainers().size(); ind2++) {
-        // System.out.println(newTown.getTrainers().get(ind2).getName() + " "
-        // + newTown.getTrainers().get(ind2).getIdentifier());
-        // }
-        // System.out.println("ID" + hCode + "\n"
-        // + "Store\nPokemon Center\nPokemon Gym");
->>>>>>> dev/pokemon
 
         return 0;
     }
