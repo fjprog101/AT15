bac8c7fdc360053815519327d8cbe39b7e1487e6
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-03-31 22:01:54 -0400

    Add fight subcommand

--- src/main/java/org/fundacionjala/at15/pokemon/commands/behavior/Fight.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/behavior/Fight.java
@@ -1,6 +1,11 @@
 package org.fundacionjala.at15.pokemon.commands.behavior;
 
+
 import java.util.concurrent.Callable;
+
+import org.fundacionjala.at15.pokemon.CurrentEntities;
+import org.fundacionjala.at15.pokemon.io.Reader;
+import static org.fundacionjala.at15.pokemon.io.Writer.*;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -12,7 +17,13 @@ public class Fight implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        System.out.println("Your business logic here....");
+        if (entityID != null) {
+            CurrentEntities current = (CurrentEntities) Reader.readJson("crt-12345678");
+            current.setPokemonOpponent(entityID);
+            writeToJson(current);
+        } else {
+            System.out.println("A Pokemon Wild ID is needed");
+        }
         return 0;
     }
 }
