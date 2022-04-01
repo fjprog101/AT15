ea2b662156f11d89790027abf4c36fda95520532
mauricio <mauridav377@gmail.com>
2022-04-01 19:41:35 -0400

    Added test class to Learn command

--- src/test/java/org/fundacionjala/at15/pokemon/commands/SubCommandLearnTest.java
+++ src/test/java/org/fundacionjala/at15/pokemon/commands/SubCommandLearnTest.java
@@ -1,24 +1,32 @@
 package org.fundacionjala.at15.pokemon.commands;
 import static org.junit.Assert.assertEquals;
 
+import org.fundacionjala.at15.pokemon.Pokemon;
 import org.fundacionjala.at15.pokemon.commands.behavior.Learn;
+import org.fundacionjala.at15.pokemon.commands.behavior.Select;
 import org.fundacionjala.at15.pokemon.commands.create.CreatePokemon;
 import org.fundacionjala.at15.pokemon.io.Reader;
+import org.junit.Test;
+
 import picocli.CommandLine;
 
 public class SubCommandLearnTest {
-    //@Test
+    @Test
     public void commandShouldCreateAddMovementToJsonFile() {
-        CreatePokemon pokemon1 = new CreatePokemon();
-        new CommandLine(pokemon1).parseArgs("-n", "pikachu", "-hp", "200");
-        pokemon1.call();
+        CreatePokemon poke = new CreatePokemon();
+        new CommandLine(poke).parseArgs("-n", "squirle", "-hp", "100");
+        poke.call();
 
-        Learn subCommandLearn = new Learn();
-        new CommandLine(subCommandLearn).parseArgs("-ID", pokemon1.getIdPokemonCreated());
-        subCommandLearn.call();
+        Select select = new Select();
+        new CommandLine(select).parseArgs("-id", poke.getIdPokemonCreated());
+        select.call();
 
-        Reader.readJson(subCommandLearn.getIdPOkemon());
-        String expected = "id: " + subCommandLearn.getIdPOkemon() + " name: pikachu hitpoints: 200/200";
-        assertEquals(expected, Reader.getResult());
+        Learn learn = new Learn();
+        new CommandLine(learn).parseArgs("-name", "Patada", "-damage", "40");
+        learn.call();
+
+        Pokemon pokemon = (Pokemon) Reader.readJson(learn.getIdPOkemon());
+        String expected = pokemon.getMove("Patada").getMovement();
+        assertEquals("Patada", expected);
     }
 }
