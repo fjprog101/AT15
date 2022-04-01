6f314a5c05df9649f59b9ba6ab8549789d5a9351
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-03-31 21:01:27 -0400

    Create CreateCurrentEntities command CLI

---
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/create/CreateCurrentEntities.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.at15.pokemon.commands.create;
+import static org.fundacionjala.at15.pokemon.io.Writer.*;
+
+import java.util.concurrent.Callable;
+
+import org.fundacionjala.at15.pokemon.CurrentEntities;
+
+import picocli.CommandLine.Command;
+
+@Command(name = "current", description = "Create a new current entities json file")
+public class CreateCurrentEntities implements Callable<Integer> {
+    
+    @Override
+    public Integer call() {
+        CurrentEntities currentEntities = new CurrentEntities();
+        writeToJson(currentEntities);
+        System.out.println("File created successfully");
+        return 0;
+    }
+}
