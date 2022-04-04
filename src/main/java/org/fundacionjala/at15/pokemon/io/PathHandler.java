018c095bb2f95cbfd69888449f8bc20f155e0b63
Adrian Oviedo <adryoviedo@gmail.com>
2022-04-04 14:54:10 -0400

    refactor the PathHandler logic

--- src/main/java/org/fundacionjala/at15/pokemon/io/PathHandler.java
+++ src/main/java/org/fundacionjala/at15/pokemon/io/PathHandler.java
@@ -5,24 +5,55 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.fundacionjala.at15.pokemon.io.EntityType.*;
+import static org.fundacionjala.at15.pokemon.io.Query.*;
 
 public class PathHandler {
     private static final int THREE = 3;
+    public static String getTypeFile(String name) {
+        String path = getSubPath(name);
+        ArrayList<String> fileNames = getFiles(path);
+        String selectedFile = "";
+        for (String names : fileNames) {
+            if (names.contains(name)) {
+                selectedFile = names;
+            }
+        }
+        String result = "";
+        if (selectedFile.contains(".json")) {
+            result = "json";
+        }
+        if (selectedFile.contains(".xml")) {
+            result = "xml";
+        }
+        return result;
+    }
+
     public static File getPath(String fileName) {
-        String subName = fileName.substring(0, THREE);
+        String path = getSubPath(fileName);
+        return new File(path + "/" + fileName + ".json");
+    }
+
+    public static File getPathXML(String fileName) {
+        String path = getSubPath(fileName);
+        return new File(path + "/" + fileName + ".xml");
+    }
+
+    public static String getSubPath(String name) {
+        String subName = name.substring(0, THREE);
         switch (subName) {
             case "pkm":
-                return new File(POKEMON.getPath() + "/" + fileName + ".json");
+                return POKEMON.getPath();
             case "trn":
-                return new File(TRAINER.getPath() + "/" + fileName + ".json");
+                return TRAINER.getPath();
             case "btt":
-                return new File(BATTLE.getPath() + "/" + fileName + ".json");
+                return BATTLE.getPath();
             case "twn":
-                return new File(TOWN.getPath() + "/" + fileName + ".json");
+                return TOWN.getPath();
             case "crt":
-                return new File(CURRENT.getPath() + "/" + fileName + ".json");
+                return CURRENT.getPath();
             default:
                 return null;
         }
