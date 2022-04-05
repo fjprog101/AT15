272c478700132e9669c2a95dd4f631337177f2ad
Adrian Oviedo <adryoviedo@gmail.com>
2022-04-04 22:20:19 -0400

    started XML reader class

---
+++ src/main/java/org/fundacionjala/at15/pokemon/io/ReaderXML.java
@@ -0,0 +1,34 @@
+package org.fundacionjala.at15.pokemon.io;
+
+import org.fundacionjala.at15.pokemon.*;
+
+public class ReaderXML extends Reader {
+    public static Entity readXML(String subName, String line) {
+        //JAXBContext jaxbContext;
+        /*if (subName.equals("pkm")) {
+
+            parsePokemonObject(entity);
+            return entity;
+        }
+        if (subName.equals("trn")) {
+
+            parseTrainerObject(entity);
+            return entity;
+        }
+        if (subName.equals("btt")) {
+
+            parseTrainerBattleObject(entity);
+            return entity;
+        }
+        if (subName.equals("twn")) {
+
+            parseTownObject(entity);
+            return entity;
+        }
+        if (subName.equals("crt")) {
+
+            return entity;
+        }*/
+        return null;
+    }
+}
