fc618b09618071d43884145d4885881ae3a63d9b
adhes14 <adhemar.duran@fundacion-jala.org>
2022-04-05 08:31:33 -0400

    Create xml writer class which extends from writer

---
+++ src/main/java/org/fundacionjala/at15/pokemon/io/XmlWriter.java
@@ -0,0 +1,12 @@
+package org.fundacionjala.at15.pokemon.io;
+
+import org.fundacionjala.at15.pokemon.Entity;
+
+public class XmlWriter extends Writer {
+
+    @Override
+    public void writeToFile(Entity entity) {
+        
+    }
+    
+}
