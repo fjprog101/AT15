8175a29fd20e236b1eab29fb2521658f13f900d6
adhes14 <adhemar.duran@fundacion-jala.org>
2022-04-04 21:26:30 -0400

    Change path name to subPath

--- src/main/java/org/fundacionjala/at15/pokemon/io/Writer.java
+++ src/main/java/org/fundacionjala/at15/pokemon/io/Writer.java
@@ -8,12 +8,12 @@ import org.fundacionjala.at15.pokemon.Entity;
 
 public final class Writer {
     public static void writeToJson(Entity entity) {
-        File path = new File(entity.getType().getPath());
-        PathHandler.checkFolder(path);
+        File subPath = new File(entity.getType().getPath());
+        PathHandler.checkFolder(subPath);
         String fileName = entity.getId();
         String jsonString = new Gson().toJson(entity);
         try {
-            FileWriter file = new FileWriter(path + "/" + fileName + ".json");
+            FileWriter file = new FileWriter(subPath + "/" + fileName + ".json");
             file.write(jsonString);
             file.flush();
             file.close();
