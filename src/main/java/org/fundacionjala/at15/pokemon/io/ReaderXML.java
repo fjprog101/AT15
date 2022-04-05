b2f5164617c22a755841bda550af0c3ac604f6b6
Adrian Oviedo <adryoviedo@gmail.com>
2022-04-05 09:45:26 -0400

    first implementation of ReaderXML

--- src/main/java/org/fundacionjala/at15/pokemon/io/ReaderXML.java
+++ src/main/java/org/fundacionjala/at15/pokemon/io/ReaderXML.java
@@ -2,9 +2,24 @@ package org.fundacionjala.at15.pokemon.io;
 
 import org.fundacionjala.at15.pokemon.*;
 
+import java.io.*;
+
+import javax.xml.bind.*;
+
 public class ReaderXML extends Reader {
     public static Entity readXML(String subName, String line) {
-        //JAXBContext jaxbContext;
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(Pokemon.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            Pokemon pokemon = (Pokemon) jaxbUnmarshaller.unmarshal(new StringReader(line));
+
+            System.out.println(pokemon);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
         /*if (subName.equals("pkm")) {
 
             parsePokemonObject(entity);
