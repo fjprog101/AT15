f069d64c4eb2842446190a535ee2aa52166b5d67
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-04-05 21:25:03 -0300

    Added xml flag to create Pokemon command

--- src/main/java/org/fundacionjala/at15/pokemon/commands/create/CreatePokemon.java
+++ src/main/java/org/fundacionjala/at15/pokemon/commands/create/CreatePokemon.java
@@ -3,6 +3,7 @@ package org.fundacionjala.at15.pokemon.commands.create;
 import org.fundacionjala.at15.pokemon.Pokemon;
 import org.fundacionjala.at15.pokemon.commands.exceptions.IncompleteArguments;
 
+import org.fundacionjala.at15.pokemon.io.XmlWriter;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
@@ -18,16 +19,24 @@ public class CreatePokemon implements Callable<Integer> {
     @Option(names = { "-hitPoints", "-hp" }, description = "pokemon Hit-Points")
     private int hitPoints;
 
+    @Option(names = { "--xml" }, description = "create the object in xml format")
+    private boolean xml;
+
     @Override
     public Integer call() throws IncompleteArguments {
         if ((hitPoints <= 0) || (pokemonName == null)) {
             throw new IncompleteArguments(
-                    "Error. Incomplete arguments for the create pokemon command. Required arguments: -name -hitpoints.",
+                    "Error. Incomplete arguments for the create pokemon command. Required arguments: -name -hitPoints.",
                     null, true, false);
         }
         newPokemon = new Pokemon(hitPoints, pokemonName);
         this.idPokemonCreated = newPokemon.getId();
-        newPokemon.write();
+        if (!xml) {
+            newPokemon.write();
+        } else {
+            newPokemon.write(new XmlWriter());
+        }
+
         System.out.println(
                 "Pokemon Created: \n"
                         + "Name: " + newPokemon.getPokemonName() + "\n"
